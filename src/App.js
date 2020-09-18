c38092f51cef864775f7532832393d040600be3e
Rennan Rocha Ribas <rennanrr@hotmail.com>
2020-09-18 00:31:02 -0300

    add routes and cnpj screen (exercises 1 and 2)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,63 @@
-import React, { Fragment, Suspense } from 'react';
-import { Link, NavLink, Route, withRouter } from 'react-router-dom';
-import Home from './Screens/Home';
+import React, { Fragment, Suspense, lazy } from 'react';
+import { NavLink, Redirect, Route, withRouter } from 'react-router-dom';
+import logo from './logo.svg';
 
-function App(props) {
-  console.log(props.location);
+const Home = lazy(() => import('./Screens/Home'));
+const Cnpj = lazy(() => import('./Screens/Cnpj'));
+const Rectangles = lazy(() => import('./Screens/Rectangles'));
+
+function LoadMessage(props) {
+  return (
+    <div className="loader-container">
+      <div className="loader-container-inner">
+          <h6 className="mt-5">
+            Loading {props.name} Screen
+            <small>You are almost there...</small>
+            <img src={logo} className="App-logo" alt="logo" />
+          </h6>
+      </div>
+    </div>
+  )
+}
+
+function App() {
   return (
     <div className="">
       <header className="topnav">
-        <NavLink exact to="/">Home</NavLink>
-        <NavLink to="/cpnj">CNPJ</NavLink>
+        <NavLink to="/home">Home</NavLink>
+        <NavLink to="/cnpj">CNPJ</NavLink>
         <NavLink to="/rectangles">Rectangles</NavLink>
         <NavLink to="/todolist">To Do List</NavLink>
         <NavLink to="/worldclock">World Clock</NavLink>
         <NavLink to="/diagram">Diagram (Order Management)</NavLink>
       </header>
       <Fragment>
+
+        {/* Home Screen */}
         <Suspense fallback={
-            <div className="loader-container">
-                <div className="loader-container-inner">
-                    <h6 className="mt-5">
-                        Carregando
-                        <small>Quase lá</small>
-                    </h6>
-                </div>
-            </div>
+          <LoadMessage name={"Home"}></LoadMessage>
         }>
-            <Route path="/" component={Home} />
+            <Route path="/home" component={Home} />
         </Suspense>
+
+        {/* CNPJ Validation Screen */}
+        <Suspense fallback={
+          <LoadMessage name={"CNPJ Validation"}></LoadMessage>
+        }>
+            <Route path="/cnpj" component={Cnpj} />
+        </Suspense>
+
+        {/* CNPJ Validation Screen */}
+        <Suspense fallback={
+          <LoadMessage name={"Rectangles"}></LoadMessage>
+        }>
+            <Route path="/rectangles" component={Rectangles} />
+        </Suspense>
+
+        {/* In case of root '/' path, redirect to home */}
+        <Route exact path="/" render={() => (
+                <Redirect to="/home"/>
+        )}/>
       </Fragment>
     </div>
   );
---
+++ src/Screens/CNPJ.js
@@ -0,0 +1,62 @@
+import React, { Fragment, useState } from 'react';
+
+function Cnpj() {
+
+  const [validation, setValidation] = useState('');
+  const [message, setMessage] = useState('');
+
+  const isValid = (props) => {
+    const cnpj = props.target.value;
+    //Check format (Mask and first exercise)
+    if (cnpj.match(/(^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$)/) || cnpj.match(/^\d{14}$/)) {
+      if(isValidCD(cnpj)) {
+        setValidation(true);
+        setMessage('This is a valid CNPJ.');
+      }
+      else {
+        setValidation(false);
+        setMessage(`The 'Check Digits' of this CNPJ is invalid.`);
+      }
+    }
+    else{
+      if(cnpj === '') {
+        setValidation('');
+        setMessage('');
+      }
+      else {
+        setValidation(false);
+        setMessage('Invalid format');
+      }
+    }
+  };
+
+  const isValidCD = (cnpj) => {
+      const arrayCnpj = cnpj.replace(/\D/g,'').split('');
+      const arrayWeight = [[5,4,3,2,9,8,7,6,5,4,3,2], [6,5,4,3,2,9,8,7,6,5,4,3,2]];
+      var calcCD = [0, 0];
+
+
+      for(let x = 0; x < arrayWeight.length; x++) {
+        for(let y = 0; y < arrayWeight[x].length; y++) {
+          calcCD[x] += arrayWeight[x][y] * arrayCnpj[y];
+        }
+        calcCD[x] = 11 - ((calcCD[x]%11) < 2 ? 0 : calcCD[x]%11);
+      }
+      return (calcCD[0] == arrayCnpj[12] && calcCD[1] == arrayCnpj[13]) ? true : false;
+  }
+
+  return (
+    <Fragment>
+      <div className="App">
+        <h1>CNPJ Validate</h1>
+        <p>(Exercises One and Two)</p>
+        <div className="input">
+          <input type="text" className={validation === '' ? '' : validation ? 'input-valid' : 'input-invalid'} onChange={isValid}></input>
+          <p className={validation?'message-valid':'message-invalid'}>{message}</p>
+        </div>
+      </div>
+    </Fragment>
+  )
+}
+
+export default Cnpj;
