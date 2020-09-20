a037c0472042429bdf4d333c68768784627b4751
Rennan Rocha Ribas <rennanrr@hotmail.com>
2020-09-20 00:35:38 -0300

    n rectangles intersecting, n areas calculating, and input on screen

--- src/Screens/Rectangles.js
+++ src/Screens/Rectangles.js
@@ -1,6 +1,13 @@
 import React, { Fragment, useState } from 'react';
 
 function arrayR(A, B) {
+  const gridSize = [16, 16];
+  let grid = [];
+  for(let x = 0; x < gridSize[0]; x++) {
+    for(let y = 0; y < gridSize[1]; y++) {
+      grid = [...grid, [x,y]];
+    }
+  }
   const littleA = [A[0][0] < A[1][0] ? A[0][0] : A[1][0], A[0][1] < A[1][1] ? A[0][1] : A[1][1]];
   let arrayA = [];
   for(let x = littleA[0]; x < (littleA[0] + Math.abs(A[0][0]-A[1][0])); x++){
@@ -18,76 +25,153 @@ function arrayR(A, B) {
   return (littleA,littleB);
 }
 
-//Exercise Four
-function AreaOfIntersection(a, b) {
-  let intersection = a.x1 < b.x1 ? {x1: b.x1, x2: a.x2} : {x1: a.x1, x2: b.x2};
-      intersection = a.y1 < b.y1 ? {...intersection, y1: b.y1, y2: a.y2} : {...intersection, y1: a.y1, y2: b.y2};
-  return (intersection.x2 - intersection.x1 + 1) * (intersection.y2 - intersection.y1 + 1);
-}
 
-//Exercise Three
-function Intersects(rectangles) {
-  //Sort axes by x1,x2, y1,y2 for each form
-  rectangles.map((item, i ) => {
-    let key = Object.keys(item)[0];
-    item = item[key];
-    rectangles[i][key] = item[0][0] <= item[1][0] ? 
-      {x1: item[0][0], x2: item[1][0]} 
-    : 
-      {x1: item[1][0], x2: item[0][0]};
+const Rectangles = () => {
 
-      rectangles[i][key] = item[0][1] <= item[1][1] ? 
-      {...rectangles[i][key], y1: item[0][1], y2: item[1][1]} 
-    : 
-      {...rectangles[i][key], y1: item[1][1], y2: item[0][1]};
-  });
-  let intersects = [];
-  let intersectedAll = true;
-  rectangles.map((item, i) => {
-    let key = Object.keys(item)[0];
-    item = item[key];
-    rectangles.map((item2, i2) => {
-      let key2 = Object.keys(item2)[0];
-      item2 = item2[key2];
-      if(item !== item2) {
-        if(item.x1 <= item2.x2 && item2.x1 <= item.x2 &&
-          item.y1 <= item2.y2 && item2.y1 <= item.y2)
-          intersects = [...intersects, `${key}∩${key2}`];
-        else
-          intersectedAll = false;
-      }
-    })
-  });
+  let name = 'A';
+  let r1x = 3;
+  let r1y = 5;  
+  let r2x = 11;
+  let r2y = 11;  
 
-  return {rectangles, intersects, intersectedAll};
-}
+  const [list, setList] = useState([]);
+  const [message, setMessage] = useState('');
 
-function Rectangles() {
-  const gridSize = [16, 16];
-  let grid = [];
-  for(let x = 0; x < gridSize[0]; x++) {
-    for(let y = 0; y < gridSize[1]; y++) {
-      grid = [...grid, [x,y]];
-    }
+  const calculate = () => {
+    const result = intersects(list);
+    const area = result.intersected ? areaOfIntersection(result.rectangles) : 0;
+    console.log(result);
+    console.log(area);
+    window.alert(area);
   }
-  const [A, setA] = useState([[11,11], [3,5]]);
-  const [B, setB] = useState([[7,2], [13,7]]);
-  const [C, setC] = useState([[11,6], [15,13]]);
-  const [D, setD] = useState([[1,1], [20,20]]);
-
-  console.log(Intersects([{A}, {B}, {C}, {D}]));
 
   return (
     <Fragment>
       <div className="App">
         <h1>Rectangles Intersect</h1>
         <p>(Exercises Three and Four)</p>
-        <table className="">
-          
-          </table>
+        <div>
+          <input type="text" onChange={e => name = e.target.value} placeholder="A"></input>
+          = (
+          <input type="number" className="input-number" onChange={e => r1x = e.target.valueAsNumber} placeholder="0"></input>, 
+          <input type="number" className="input-number" onChange={e => r1y = e.target.valueAsNumber} placeholder="0"></input>; 
+          <input type="number" className="input-number" onChange={e => r2x = e.target.valueAsNumber} placeholder="0"></input>,
+          <input type="number" className="input-number" onChange={e => r2y = e.target.valueAsNumber} placeholder="0"></input>)
+          <button onClick={() => {
+            setList([...list, {
+              [name]: [
+                [r1x,r1y],
+                [r2x,r2y]
+              ]
+            }]);
+
+          }}>Add</button>
+        </div>
+        {
+          list.map((item, i) => {
+            const key = Object.keys(item)[0];
+            item = item[key];
+            return (
+              <div key={i}>
+                {key} = ({item[0][0]}, {item[0][1]}; {item[1][0]}, {item[1][1]})
+              </div>
+            )
+          })
+        }
+        <button onClick={calculate}>Calculate</button>
+      <div>{message}</div>
       </div>
+
     </Fragment>
   )
+}
+
+//Exercise Four
+const areaOfIntersection = (rectangles) => {
+  let arrayX1 = [], arrayX2 = [], arrayY1 = [], arrayY2 = [];
+  rectangles.forEach((item) => {
+    let key = Object.keys(item)[0];
+    item = item[key];
+    arrayX1 = [...arrayX1, item.x1];
+    arrayX2 = [...arrayX2, item.x2];
+    arrayY1 = [...arrayY1, item.y1];
+    arrayY2 = [...arrayY2, item.y2];
+    console.log(item);
+  });
+  let intersPoints = { x1:0, x2: 0, y1:0, y2:0 };
+  //Sort the smallests (x1) points of X axe and get the bigger, 
+  //because this is the x1 point of interaction rectangle
+  intersPoints.x1 = arrayX1.sort((a,b)=>a-b)[arrayX1.length - 1];
+
+  //Sort the biggests (x2) points of X axe and get the smallest, 
+  //because this is the x2 point of interaction rectangle
+  intersPoints.x2 = arrayX2.sort((a,b)=>a-b)[0];
+
+  //Sort the smallests (y1) points of Y axe and get the bigger, 
+  //because this is the y1 point of interaction rectangle
+  intersPoints.y1 = arrayY1.sort((a,b)=>a-b)[arrayY1.length - 1];
+
+  //Sort the biggests (y2) points of Y axe and get the smallest, 
+  //because this is the y2 point of interaction rectangle
+  intersPoints.y2 = arrayY2.sort((a,b)=>a-b)[0];
+
+  console.log(intersPoints);
+  const sizeX = intersPoints.x2 - intersPoints.x1 + 1;
+  const sizeY = intersPoints.y2 - intersPoints.y1 + 1;
+  const area = sizeX * sizeY;
+  
+  return area;
+}
+
+//Exercise Three
+const intersects = (rectangles) => {
+  //variables that will be used in the responde
+  let intersects = [];
+  let intersected = true;
+
+  //Map object rectangles to sort item(rectangle) axes by x1,x2, y1,y2
+  rectangles.forEach((item, i ) => {
+    //get key("A", for example) and set item to value (coordinates)
+    const key = Object.keys(item)[0];
+    item = item[key];
+
+    //organize rectangle, orderning x1,x2
+    rectangles[i][key] = item[0][0] <= item[1][0] ? 
+      {x1: item[0][0], x2: item[1][0]} 
+    : 
+      {x1: item[1][0], x2: item[0][0]};
+
+    //organize rectangle, ordening y1,y2
+    rectangles[i][key] = item[0][1] <= item[1][1] ? 
+      {...rectangles[i][key], y1: item[0][1], y2: item[1][1]} 
+    : 
+      {...rectangles[i][key], y1: item[1][1], y2: item[0][1]};
+  });
+
+  //with object rectangles organized, will sweep each item with other
+  rectangles.forEach((item, i) => {
+    //get key and set to value
+    const key = Object.keys(item)[0];
+    item = item[key];
+
+    //test each rectangle, starting i+1, to only test 1-1 one time, 
+    //example: will test A∩C, but won't test C∩A, because A∩C=C∩A
+    for(let i2 = i + 1; i2 < rectangles.length; i2++){
+      //get key and set to value
+      let item2 = rectangles[i2];
+      const key2 = Object.keys(item2)[0];
+      item2 = item2[key2];
+
+      //check if sintersect
+      if(item.x1 <= item2.x2 && item2.x1 <= item.x2 &&
+        item.y1 <= item2.y2 && item2.y1 <= item.y2)
+        intersects = [...intersects, `${key}∩${key2}`];
+      else
+        intersected = false;
+    }
+  });
+  //return three objects
+  return {rectangles, intersects, intersected};
 }
 
 export default Rectangles;
